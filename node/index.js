eb7553c9fdf5245e08cd7ad3727133ac6d2b208d
Josué Lustosa <josuelustosa.job@gmail.com>
2023-05-21 23:53:38 -0400

    feat: work in progress - exercise 05

--- node/index.js
+++ node/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-const toUpper = require("./util.js");
+const path = require("path");
+const until = require("./util.js");
 
 require("dotenv").config();
 
@@ -8,15 +9,28 @@ const PORT = process.env.PORT || 3333;
 const folder = process.argv[2];
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
-  fs.readdir(folder, (err, files) => {
-    if (err) console.log(err);
-    else {
-      files.forEach((f) => res.write(`${toUpper(f)}\n`));
-      res.end("Instituto de Computação");
-    }
-  });
+    fs.readdir(folder, (err, files) => {
+      if (err) console.log("CONSOLE FILES ", err);
+      else {
+        files.forEach((f) => res.write(`${until.createLink(f)}<br>`));
+        res.end("<br>Instituto de Computação");
+      }
+    });
+  } else {
+    const fileName = decodeURIComponent(req.url.substring(6)); // Decodifica o nome do arquivo
+    const filePath = path.join(folder, fileName);
+
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) console.log("CONSOLE DATA ", data);
+
+      res.end(data);
+    });
+  }
 });
 
 server.listen(PORT, () => {
--- node/util.js
+++ node/util.js
@@ -2,4 +2,15 @@ function toUpper(str) {
   return str.toUpperCase(str);
 }
 
-module.exports = toUpper;
+function createLink(filename) {
+  return `<a href="/${filename}">${filename}</a>`;
+}
+function createVoltar() {
+  return `<a href="/">Voltar</a>`;
+}
+
+module.exports = {
+  toUpper,
+  createLink,
+  createVoltar
+};
